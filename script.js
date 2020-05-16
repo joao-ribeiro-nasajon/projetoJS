f463d60559a5f076c8bb939f1f4b1adf3c6aa253
joao-ribeiro-nasajon <joao-ribeiro-nasajon@github.com>
2020-05-15 23:49:03 -0300

    Adiciona projeto

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 class MathDelegate {
 
-    static quadrado = (n) => Math.pow(n, 2);
+    static quadrado = (n) => n != 0 ? Math.pow(n, 2): 0;
 
-    static cubo = (n) => Math.pow(n, 3);
+    static cubo = (n) => n != 0 ? Math.pow(n, 3) : 0;
 
     static ePrimo = function (n) {
     
+        if (n == 0 || n == 1) return "Não";
+
         for (let i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i === 0) return "Não";
+            if (n % i == 0) return "Não";
         }
 
         return "Sim";
 
     }
 
-    static fatorial = (n) => n === 0 || n === 1?
-                                1 : n * fatorial(n - 1);
+    static fatorial = (n) => n == 0 || n == 1?
+                                1 : n * MathDelegate.fatorial(n - 1);
 
-    static raiz = (n) => Math.sqrt(n);
+    static raiz = (n) => n != 0 ? Math.sqrt(n) : 0;
 
-    static parOuImpar = (n) => n % 2 === 0?
+    static parOuImpar = (n) => n % 2 == 0 || n == 0?
                                    "Par" : 'Ímpar';
 
 }
 
-class Generator {
+class QuadrosFactory {
 
     static #mathDel = [
         MathDelegate.quadrado,
@@ -44,41 +46,56 @@ class Generator {
         "Par ou ímpar?"
     ];
 
-    static gerarQuadroFuncao(n) {
-
-        let resultTitle = Generator.#funcTitles[n];
-
-        let input = document.getElementById("numInput").value;
-        let result = Generator.#mathDel[n](input);
-        document.getElementById("secaoIterativa").innerHTML = Generator.construirTemplateQuadroFuncao(resultTitle, result, n);
-
-    }
-
-    static constroiQuadros() {
-        
-        let quadros = [];
-
-        for (let i = 0; i < 6; i++) {
-            
-            let quadro = this.gerarQuadroFuncao(i);
-            
-            quadros.push(quadro);
-
-            if(i === 3 && i === 6) {
-                this.encapsulaQuadrosEmSecoes(quadros);
-                quadros = [];
-            }
-        }
-
-    }
-
-    static encapsulaQuadrosEmSecoes = (quadros) => "<div style='display: block;'>" + quadros.reduce((a, b) => '\n' + a + '\n' + b + '\n') + "</div>"; 
-
-    static construirTemplateQuadroFuncao = (funcTitle, result) =>
-        `<div class="caixa-input">
+    static #constroiTemplateQuadroFuncao = (funcTitle, result) =>
+        `<div class="caixa caixa-output">
             <p>${funcTitle}</p>
             <p>${result}</p>
         </div>`;
 
+    static #geraQuadroFuncao = function (n) {
+
+        let resultTitle = QuadrosFactory.#funcTitles[n];
+
+        let input = document.getElementById("numInput").value;
+
+        let result = QuadrosFactory.#mathDel[n](input);
+        
+        return QuadrosFactory.#constroiTemplateQuadroFuncao(resultTitle, result);
+
+    }
+
+    static #encapsulaQuadrosEmLinhas = (quadros) => "<div class='linha'>" + quadros.reduce((a, b) => '\n' + a + '\n' + b + '\n') + "</div>"; 
+
+    static #constroiQuadros = function () {
+        
+        let output = "";
+
+        let quadros = [];
+
+        for (let i = 0; i < 6; i++) {
+            
+            let quadro = QuadrosFactory.#geraQuadroFuncao(i);
+            
+            quadros.push(quadro);
+
+            if(i === 2 || i === 5) {
+
+                output += "\n" + QuadrosFactory.#encapsulaQuadrosEmLinhas(quadros);
+                quadros = [];
+
+            }
+
+        }
+
+        return output;
+
+    }
+
+    static desenhaQuadros = function () {
+
+        let quadrosConstruidos = QuadrosFactory.#constroiQuadros();
+        document.getElementById("secaoIterativa").innerHTML = QuadrosFactory.#constroiQuadros();
+
+    }
 
 }
